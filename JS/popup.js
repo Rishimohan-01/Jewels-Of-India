4f809f5b06bc14ec038e2f24e30ba813d7980430
Rishimohan-01 <mr.rishimohan.01@gmail.com>
2024-09-12 17:08:43 +0530

    ui complete for desktop

--- JS/popup.js
+++ JS/popup.js
@@ -1,4 +1,12 @@
-function toggle() {
-  var blur = document.getElementById("blur");
-  blur.classList.toggle("active");
-}
+// Get the modal element
+const floorPlanModal = document.getElementById("floor-plan-modal");
+const knowMoreModal = document.getElementById("know-more-modal");
+
+// Add an event listener to the button to open the modal
+document.getElementById("floor-plan-btn").addEventListener("click", () => {
+  floorPlanModal.modal("show");
+});
+
+document.getElementById("know-more-btn").addEventListener("click", () => {
+  knowMoreModal.modal("show");
+});
