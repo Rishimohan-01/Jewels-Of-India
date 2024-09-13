9d8ece2868502ece10dd368cb9975960b8a5a8f6
Rishimohan-01 <mr.rishimohan.01@gmail.com>
2024-09-13 17:58:59 +0530

    comment slider script and iframe

--- JS/slider.js
+++ JS/slider.js
@@ -1,4 +1,4 @@
-var copy1 = document.querySelector(".company-logo").cloneNode(true);
-document.querySelector(".exhibitor-features").appendChild(copy1);
-var copy2 = document.querySelector(".company-logo").cloneNode(true);
-document.querySelector(".exhibitor-features").appendChild(copy2);
+// var copy1 = document.querySelector(".company-logo").cloneNode(true);
+// document.querySelector(".exhibitor-features").appendChild(copy1);
+// var copy2 = document.querySelector(".company-logo").cloneNode(true);
+// document.querySelector(".exhibitor-features").appendChild(copy2);
